cb55b7dfa938091eec247a55e2d2bdc4fc3b94c6
Ruben van der Linde <rubenvdlinde@gmail.com>
2020-06-30 22:59:49 +0000

    Apply fixes from StyleCI

--- api/src/DataFixtures/WestfrieslandFixtures.php
+++ api/src/DataFixtures/WestfrieslandFixtures.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Organization;
-use Conduction\CommonGroundBundle\CommonGroundBundle;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -107,7 +106,5 @@ class WestfrieslandFixtures extends Fixture
         $koggenland = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
 
         $manager->flush();
-
-
     }
 }
