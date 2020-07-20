dc32365b359f63fcfd1c399bf6da2e7fd7172c70
ginokok1996 <ginokok1996@hotmail.com>
2020-07-20 21:29:25 +0200

    added zuid-drecht fixtures & added a person

---
+++ api/src/DataFixtures/ZuidDrechtFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Person;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class ConductionFixtures extends Fixture
+{
+    private $params;
+    /**
+     * @var CommonGroundService
+     */
+    private $commonGroundService;
+
+    public function __construct(ParameterBagInterface $params, CommonGroundService $commonGroundService)
+    {
+        $this->params = $params;
+        $this->commonGroundService = $commonGroundService;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        if (
+            $this->params->get('app_domain') != 'zuid-drecht.nl' &&
+            strpos($this->params->get('app_domain'), 'zuid-drecht.nl') == false &&
+            $this->params->get('app_domain') != 'zuiddrecht.nl' &&
+            strpos($this->params->get('app_domain'), 'zuiddrecht.nl') == false
+        ) {
+            return false;
+        }
+        // Test Student
+        $id = Uuid::fromString('f7f9afcf-9aaf-4e36-8911-4cf1ebf3270f');
+        $person = new Person();
+        $person->setGivenName('Gino');
+        $person->setFamilyName('Kok');
+        $manager->persist($person);
+        $person->setId($id);
+        $manager->persist($person);
+        $manager->flush();
+        $person = $manager->getRepository('App:Person')->findOneBy(['id'=> $id]);
+
+        $manager->flush();
+    }
+}
