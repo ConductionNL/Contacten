dd75c3049378530e51dac8d8c875dd00a4a1c9b8
Wilco Louwerse <wilco@conduction.nl>
2020-08-06 16:05:03 +0200

    Added Parent and suborganization(s) and updated datamodel
    Also added datafixtures for stage

--- api/src/DataFixtures/ConductionFixtures.php
+++ api/src/DataFixtures/ConductionFixtures.php
@@ -52,52 +52,106 @@ class ConductionFixtures extends Fixture
         //Partners
         // Conduction
         $id = Uuid::fromString('9650a44d-d7d1-454a-ab4f-2338c90e8c2f');
-        $westfriesland = new Organization();
-        $westfriesland->setName('Conduction');
-        $westfriesland->setDescription('Conduction');
-        $westfriesland->setType('Partner');
-        $manager->persist($westfriesland);
-        $westfriesland->setId($id);
-        $manager->persist($westfriesland);
+        $conduction = new Organization();
+        $conduction->setName('Conduction');
+        $conduction->setDescription('Conduction');
+        $conduction->setType('Partner');
+        $manager->persist($conduction);
+        $conduction->setId($id);
+        $manager->persist($conduction);
         $manager->flush();
-        $westfriesland = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+        $conduction = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
 
         // VNG
         $id = Uuid::fromString('80a987a0-a5e0-4aa0-bd90-a931871d9283');
-        $westfriesland = new Organization();
-        $westfriesland->setName('VNG');
-        $westfriesland->setDescription('Vereniging van Nederlandse Gemeenten');
-        $westfriesland->setType('Partner');
-        $manager->persist($westfriesland);
-        $westfriesland->setId($id);
-        $manager->persist($westfriesland);
+        $vng = new Organization();
+        $vng->setName('VNG');
+        $vng->setDescription('Vereniging van Nederlandse Gemeenten');
+        $vng->setType('Partner');
+        $manager->persist($vng);
+        $vng->setId($id);
+        $manager->persist($vng);
         $manager->flush();
-        $westfriesland = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+        $vng = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
 
         // SIDN
         $id = Uuid::fromString('a30454f9-7e97-4e25-9094-245bab73cf9b');
-        $westfriesland = new Organization();
-        $westfriesland->setName('SIDN');
-        $westfriesland->setDescription('SIDN');
-        $westfriesland->setType('Partner');
-        $manager->persist($westfriesland);
-        $westfriesland->setId($id);
-        $manager->persist($westfriesland);
+        $sidn = new Organization();
+        $sidn->setName('SIDN');
+        $sidn->setDescription('SIDN');
+        $sidn->setType('Partner');
+        $manager->persist($sidn);
+        $sidn->setId($id);
+        $manager->persist($sidn);
         $manager->flush();
-        $westfriesland = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+        $sidn = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
 
         //Participanten
         // ROC Flevoland
         $id = Uuid::fromString('35e3862b-d446-4541-9780-7bfb19c40e01');
-        $westfriesland = new Organization();
-        $westfriesland->setName('ROC Flevoland');
-        $westfriesland->setDescription('ROC van Flevoland');
-        $westfriesland->setType('Participant');
-        $manager->persist($westfriesland);
-        $westfriesland->setId($id);
-        $manager->persist($westfriesland);
+        $rocFlevoland = new Organization();
+        $rocFlevoland->setName('ROC Flevoland');
+        $rocFlevoland->setDescription('ROC van Flevoland');
+        $rocFlevoland->setType('Participant');
+        $manager->persist($rocFlevoland);
+        $rocFlevoland->setId($id);
+        $manager->persist($rocFlevoland);
         $manager->flush();
-        $westfriesland = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+        $rocFlevoland = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+
+        // Test Participant
+        $id = Uuid::fromString('80403186-62c4-4986-a5ed-d58655a9a316');
+        $testParticipant = new Organization();
+        $testParticipant->setName('Test participant 1');
+        $testParticipant->setDescription('Dit is een test participant');
+        $testParticipant->setType('Participant');
+        $manager->persist($testParticipant);
+        $testParticipant->setId($id);
+        $manager->persist($testParticipant);
+        $manager->flush();
+        $testParticipant = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+
+        //Leerjaren
+        // Test Leerjaar
+        $id = Uuid::fromString('a1321118-2780-4b68-a9dc-a232c4b54fa6');
+        $testLeerjaar = new Organization();
+        $testLeerjaar->setName('Test leerjaar 1');
+        $testLeerjaar->setDescription('Dit is een test leerjaar');
+        $testLeerjaar->setType('Leerjaar');
+        $testLeerjaar->setParentOrganization($testParticipant);
+        $manager->persist($testLeerjaar);
+        $testLeerjaar->setId($id);
+        $manager->persist($testLeerjaar);
+        $manager->flush();
+        $testLeerjaar = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+
+        //Klassen
+        // Test Klas
+        $id = Uuid::fromString('ee82d0c2-84c6-4c85-ab65-49bd2339edf4');
+        $testKlas = new Organization();
+        $testKlas->setName('Test klas 1');
+        $testKlas->setDescription('Dit is een test klas');
+        $testKlas->setType('Klas');
+        $testKlas->setParentOrganization($testLeerjaar);
+        $manager->persist($testKlas);
+        $testKlas->setId($id);
+        $manager->persist($testKlas);
+        $manager->flush();
+        $testKlas = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
+
+        //Studiegroepen
+        // Test Studiegroep
+        $id = Uuid::fromString('e13df885-be51-490f-b9b3-41ebd9c5b075');
+        $testStudiegroep = new Organization();
+        $testStudiegroep->setName('Test studiegroep 1');
+        $testStudiegroep->setDescription('Dit is een test studiegroep');
+        $testStudiegroep->setType('Studiegroep');
+        $testStudiegroep->setParentOrganization($testKlas);
+        $manager->persist($testStudiegroep);
+        $testStudiegroep->setId($id);
+        $manager->persist($testStudiegroep);
+        $manager->flush();
+        $testStudiegroep = $manager->getRepository('App:Organization')->findOneBy(['id'=> $id]);
 
         $manager->flush();
     }
