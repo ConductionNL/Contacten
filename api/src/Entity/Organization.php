59bc2e01025c5a5c1daa7a820944e7d359753997
Robert Zondervan <Robert@conduction.nl>
2020-10-02 09:23:19 +0200

    Enable type filter and add email to westfriesland just in case

--- api/src/Entity/Organization.php
+++ api/src/Entity/Organization.php
@@ -62,7 +62,9 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiFilter(BooleanFilter::class)
  * @ApiFilter(OrderFilter::class)
  * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
- * @ApiFilter(SearchFilter::class)
+ * @ApiFilter(SearchFilter::class, properties={
+ *     "type": "exact"
+ *     })
  */
 class Organization
 {
